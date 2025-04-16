b99ab7df8e5761cecc25f36655c64a4f47ec3531
sanayuj <sanayuj2255@gmail.com>
2025-04-16 10:07:14 +0530

    book repo

--- repo/bookrepo.js
+++ repo/bookrepo.js
@@ -1,5 +1,6 @@
 
 
+
 class BookRepository{
     constructor(bookModel){
         this.bookModel=bookModel;
@@ -13,4 +14,34 @@ class BookRepository{
             authorName
         })
     }
+    getAllBooks(){
+        return this.bookModel.findAll();
+    }
+
+    getBookById(uuid){
+        return this.bookModel.findOne({
+            where:{
+                id:uuid
+            }
+        }).then(function(book){
+            if(!book){
+                return 'book not found';
+            }
+            return book.dataValue;
+        })
+    }
+
+    async updateBookById(uuid,updatedName,updatedReleaseDate,updatedAuthor){
+        var updatedBookObject={};
+        return this.bookModel.update({name:updatedName,releaseDate:updatedReleaseDate,authorName:updatedAuthor},{returning: true, plain: true, where: {id: uuid} }). then(function(result){
+            if(result!=null && result.length>0){
+                console. log('updated record' + result [1].dataValues) ;
+                 return result[1].dataValues;
+
+            }else{
+                return updatedBookObject
+            }
+
+        }
+        );
 }
